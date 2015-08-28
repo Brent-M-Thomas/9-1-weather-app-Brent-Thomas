38b0fc3bc6e23dd6e6cd1ac9a4c8019636643886
Brent Thomas <brentmt24@gmail.com>
2015-08-28 15:54:56 -0500

    displaying data. added icons for weather types. installed momentjs

--- app/models/show-weather.js
+++ app/models/show-weather.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  latitude: DS.attr(),
-  longitude: DS.attr(),
+  latitude: DS.attr('number'),
+  longitude: DS.attr('number'),
   currently: DS.attr(),
   hourly: DS.attr(),
   daily: DS.attr(),
